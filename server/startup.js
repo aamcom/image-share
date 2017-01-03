26e04c2d2d671885d8390bb265314a1bde59081e
Andrew Ackah-Miezan <tawala@gmail.com>
2017-01-03 02:46:44 +0100

    tidy up the project ... allow update

--- server/startup.js
+++ server/startup.js
@@ -1,18 +1,16 @@
 
-if (Meteor.isServer) {
-    console.log("startutp.js says - I am server2.bb")
-    Meteor.startup(function(){
-        if (Images.find().count() == 0) 
-            for(i=100;i<1000;i++){
-                //var st= "IMG_0"+i+".JPG";
-                var st= i+".jpg";
-                Images.insert(
-                    {
-                        img_src:st,
-                        img_alt:"ici une image n°"+i
-                    }
-                )
-                console.log("startutp.js says - st:"+st);
-            }// endfor 
-    })
-}
+console.log("startutp.js says - I am server2.bb")
+Meteor.startup(function(){
+    if (Images.find().count() == 0) 
+        for(i=100;i<1000;i++){
+            //var st= "IMG_0"+i+".JPG";
+            var st= i+".jpg";
+            Images.insert(
+                {
+                    img_src:st,
+                    img_alt:"ici une image n°"+i
+                }
+            )
+            console.log("startutp.js says - st:"+st);
+        }// endfor 
+})
